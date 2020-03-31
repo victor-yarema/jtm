c4de41f2c0356cea390b070d00c5de110807df38
Victor Yarema <victor.yarema@gmail.com>
2020-03-31 21:23:29 +0200

    Debug

--- lib/getoptions.hpp
+++ lib/getoptions.hpp
@@ -457,6 +457,7 @@ void Getopt::parseInputArgs_(int argc, char *argv[], const std::string &fmt) {
  char option;
  while(((option = getopt(argc, argv, fmt.c_str())) != -1)
 		&& (option != 255)) {      // read next option character
+cout << "Parse IA 00" << endl;
   if(option == ':')
    throw EXP(opt_argument_missing);                             // missing argument, forced ':'
 
